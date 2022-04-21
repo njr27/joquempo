cc169f12a4111e3a5835b7a45e11a4a27fdf5bc2
njr27 <nivaldojunior00@gmail.com>
2022-04-21 19:11:50 -0300

    Execução do código proposto na aula 13

--- index.js
+++ index.js
@@ -14,101 +14,142 @@ const $winnerTitle = document.querySelector('.winner-title')
 const $scoreP1 = document.querySelector('.score-p1')
 const $scoreP2 = document.querySelector('.score-p2')
 
+const $buttonReset = document.querySelector('.button-reset')
+const $buttonStart = document.querySelector('.button-start')
 
-let $scoreCountP1 = 0
-let $scoreCountP2 = 0
 
-let $movePlayer1 = ''
-let $movePlayer2 = ''
-let $gameResult = null
+let scoreCountP1 = 0
+let scoreCountP2 = 0
+
+let movePlayer1 = ''
+let movePlayer2 = ''
+let gameResult = null
+
+let gameStart = false
 
 function verifyGame() {
-    if ($movePlayer1 == 'stone' && $movePlayer2 == 'paper') {              
-        $gameResult = 2
-    } else if ($movePlayer1 == 'stone' && $movePlayer2 == 'scissors') {
-        $gameResult = 1
-    } else if ($movePlayer1 =='paper' && $movePlayer2 == 'stone'){
-        $gameResult = 1
-    }else if($movePlayer1 == 'paper' && $movePlayer2 == 'scissors'){
-        $gameResult = 2
-    }else if($movePlayer1 == 'scissors' && $movePlayer2 == 'stone'){
-        $gameResult = 2
-    }else if($movePlayer1 == 'scissors' && $movePlayer2 == 'paper'){
-        $gameResult = 1
-    }else if($movePlayer1 == $movePlayer2){
-        $gameResult = 0
+    if (movePlayer1 == 'stone' && movePlayer2 == 'paper') {              
+        gameResult = 2
+    } else if (movePlayer1 == 'stone' && movePlayer2 == 'scissors') {
+        gameResult = 1
+    } else if (movePlayer1 =='paper' && movePlayer2 == 'stone'){
+        gameResult = 1
+    }else if(movePlayer1 == 'paper' && movePlayer2 == 'scissors'){
+        gameResult = 2
+    }else if(movePlayer1 == 'scissors' && movePlayer2 == 'stone'){
+        gameResult = 2
+    }else if(movePlayer1 == 'scissors' && movePlayer2 == 'paper'){
+        gameResult = 1
+    }else if(movePlayer1 == movePlayer2){
+        gameResult = 0
     }
 }
 
 
 function printGameResult(){
 
-    if($gameResult == 0){
+    if(gameResult == 0){
         $winnerTitle.innerHTML = '<div class="animation-score-title"> Empatou </div>'
-    }else if($gameResult == 1){      
+    }else if(gameResult == 1){      
         $winnerTitle.innerHTML = 'Jogadora 1 ganhou'
-    } else if($gameResult == 2){
+    } else if(gameResult == 2){
     $winnerTitle.innerHTML = 'Jogadora 2 ganhou'
     }
 }
 
-function scoreCount(){
-    if($gameResult == 1 ){        
-    $scoreCountP1++
-    $scoreP1.innerHTML = '<div class="animation-score">' + $scoreCountP1 + '</div>'
-        
-    } else if($gameResult == 2){
-       $scoreCountP2++
-       $scoreP2.innerHTML = '<div class="animation-score">' + $scoreCountP2 + '</div>'
-    }
+function resetBattlefield(){
+    $fieldPlayer1.innerHTML = ''
+    $fieldPlayer2.innerHTML = ''
 
 }
 
+function resetMoveVariables(){
+    movePlayer1 = ''
+    movePlayer2 = ''
+}
+
+function resetScoreboard(){
+    $scoreP1.innerHTML = '00'
+    $scoreP2.innerHTML = '00'
+}
+
+function resetScoreVariables(){
+    scoreCountP1 = 0
+    scoreCountP2 = 0
+}
+
+function scorePrint(){
+    if(gameResult == 1){
+        $scoreP1.innerHTML = '<div class="animation-score">' + scoreCountP1 + '</div>'
+    }else if(gameResult == 2){
+    $scoreP2.innerHTML = '<div class="animation-score">' + scoreCountP2 + '</div>'
+    }
+}
+
+function scoreCount(){
+    if(gameResult == 1 ){        
+    scoreCountP1++ 
+    } else if(gameResult == 2){
+       scoreCountP2++
+    }
+}
+
+
+function move(moveName, movePlayer){
+  if (gameStart){
+    if(movePlayer == 1){
+        $fieldPlayer1.innerHTML = '<img  class="move-img" src="'+moveName+'.png"/>'
+        movePlayer1 = moveName
+   }else if(movePlayer == 2){
+        $fieldPlayer2.innerHTML = '<img  class="move-img-2" src="'+moveName+'.png"/>'
+        movePlayer2 = moveName
+   }
+    verifyGame()
+    printGameResult()
+    if(gameResult != null){
+        setTimeout(resetBattlefield, 2000)
+        resetMoveVariables()
+        scoreCount() 
+        scorePrint()
+        gameResult = null
+    }
+  }
+}
 
 $stoneButton.addEventListener('click', function () {
-    $fieldPlayer1.innerHTML = '<img  class="move-img" src="stone.png"/>'
-    $movePlayer1 = 'stone'
-    verifyGame()
-    printGameResult();
-    scoreCount()
-    
+    move('stone', 1)
 })
 
 $paperButton.addEventListener('click', function () {
-    $fieldPlayer1.innerHTML = '<img class="move-img" src="paper.png"/>'
-    $movePlayer1 = 'paper'
-    verifyGame()
-    printGameResult()
+    move('paper', 1)
+})
 
-scoreCount()})
 $scissorsButton.addEventListener('click', function () {
-    $fieldPlayer1.innerHTML = '<img class="move-img" src="scissors.png"/>'
-    $movePlayer1 = 'scissors'
-    verifyGame()
-    printGameResult()
-
-scoreCount()})
+    move('scissors', 1)
+})
 
 $stoneButton2.addEventListener('click', function () {
-    $fieldPlayer2.innerHTML = '<img class="move-img-2" src="stone.png"/>'
-    $movePlayer2 = 'stone'
-    verifyGame()
-    printGameResult()
-
-scoreCount()})
+    move('stone', 2)
+})
 
 $paperButton2.addEventListener('click', function () {
-    $fieldPlayer2.innerHTML = '<img class="move-img-2" src="paper.png"/>'
-    $movePlayer2 = 'paper'
-    verifyGame()
-    printGameResult()
-    scoreCount()
+    move('paper', 2)
 })
 
 $scissorsButton2.addEventListener('click', function () {
-    $fieldPlayer2.innerHTML = '<img class="move-img-2" src="scissors.png"/>'
-    $movePlayer2 = 'scissors'
-    verifyGame()
-    printGameResult()
+    move('scissors', 2)
+})
 
-scoreCount()})
+$buttonReset.addEventListener('click', function (){
+    resetBattlefield()
+    resetMoveVariables()
+    resetScoreboard()
+    resetScoreVariables()
+    gameResult = null
+    $winnerTitle.innerHTML = 'Vencedor'
+})
+
+$buttonStart.addEventListener('click', function(){
+    gameStart = !gameStart
+    $buttonStart.classList.toggle('start')
+})
